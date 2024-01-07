600d8ac970b5ae0427a5171833a63080ecd98e54
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-08 00:08:08 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,16 @@
+def solution(N, cp_list):
+    distance_list = []
+    for idx in range(N-1):
+        distance = abs(cp_list[idx][0] - cp_list[idx+1][0]) + abs(cp_list[idx][1] - cp_list[idx+1][1])
+        distance_list.append(distance)
+    longest_distance_idx = distance_list.index(max(distance_list)) + 1
+    del cp_list[longest_distance_idx]
+    distance_list = []
+    for idx in range(N-2):
+        distance = abs(cp_list[idx][0] - cp_list[idx+1][0]) + abs(cp_list[idx][1] - cp_list[idx+1][1])
+        distance_list.append(distance)
+    return sum(distance_list)
+
+N = int(input())
+cp_list = [list(map(int, input().split())) for _ in range(N)]
+print(solution(N, cp_list))

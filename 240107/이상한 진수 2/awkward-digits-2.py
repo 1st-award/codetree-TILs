8cc88a48a84bcc536598d096987694a4a0bb6fa5
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-07 23:46:20 +0900

    Title: 이상한 진수 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240107/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,17 @@
+def solution(num_str):
+    bin_pos = 1
+    bin_sum = 0
+    for idx in range(1, len(num_str)):
+        if num_str[idx] == "0":
+            num_str[idx] = "1"
+            break
+
+    num_str.reverse()
+    for ch in num_str:
+        if ch == "1":
+            bin_sum += bin_pos
+        bin_pos *= 2
+    return bin_sum
+
+num_str = list(input())
+print(solution(num_str))

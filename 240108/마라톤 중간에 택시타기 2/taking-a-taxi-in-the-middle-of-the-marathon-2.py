2001014da1098da570835044b897f645b1b47760
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-08 00:29:52 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240108/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240108/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,16 +1,17 @@
-def solution(N, cp_list):
-    distance_list = []
-    for idx in range(N-2):
-        distance = abs(cp_list[idx][0] - cp_list[idx+1][0]) + abs(cp_list[idx][1] - cp_list[idx+1][1])
-        distance_list.append(distance)
-    longest_distance_idx = distance_list.index(max(distance_list)) + 1
-    del cp_list[longest_distance_idx]
-    distance_list = []
-    for idx in range(N-2):
-        distance = abs(cp_list[idx][0] - cp_list[idx+1][0]) + abs(cp_list[idx][1] - cp_list[idx+1][1])
-        distance_list.append(distance)
-    return sum(distance_list)
+import sys
 
+def solution(N, cp_list):
+    min_disatnce = MAX_NUM
+    for skip_cp in range(1, N-1):
+        temp_distance = 0
+        temp_cp_list = cp_list.copy()
+        del temp_cp_list[skip_cp]
+        for cp_idx in range(N - 2):
+            temp_distance += abs(temp_cp_list[cp_idx][0] - temp_cp_list[cp_idx+1][0]) + abs(temp_cp_list[cp_idx][1] - temp_cp_list[cp_idx+1][1])
+        min_disatnce = min(temp_distance, min_disatnce)
+    return min_disatnce
+
+MAX_NUM = sys.maxsize
 N = int(input())
 cp_list = [list(map(int, input().split())) for _ in range(N)]
 print(solution(N, cp_list))

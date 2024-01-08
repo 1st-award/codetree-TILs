fcf3d58b9ed2bae75408ada97ac5194b41e1afc1
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-08 13:12:36 +0900

    Title: 최고의 13위치 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,15 @@
+def solution(N, board):
+    max_coin = 0
+    for i in range(N - 1):
+        for j in range(N - 2):
+            for y in range(i + 1, N - 1):
+                for x in range(N - 2):
+                    a_search = board[i][j:j+3]
+                    b_search = board[y][x:x+3]
+                    sum_a_b = a_search.count(1) + b_search.count(1)
+                    max_coin = max(max_coin, sum_a_b)
+    return max_coin
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+print(solution(N, board))

1cb88b25d786279f590e891d1bfcd10754b1e36c
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-08 15:20:42 +0900

    Title: 바구니 안의 사탕 2, Time: 192ms, Memory: 27MB, Status: Passed - CodeTree

--- 240108/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240108/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,11 +1,11 @@
 def solution(N, K, baskets):
     max_candy = 0
     for c in range(1000 - K + 1):
-        for i in range(c + 4, 1000 -K - 4 + 1):
+        for i in range(c + K, 1000 -K + 1):
             max_candy = max(max_candy, sum(baskets[i - K:i + K + 1]))
     return max_candy
 N, K = map(int, input().split())
 baskets = [0] * 101
 for candy, pos in list(map(int, input().split()) for _ in range(N)):
-    baskets[pos] = candy
+    baskets[pos] += candy
 print(solution(N, K, baskets))

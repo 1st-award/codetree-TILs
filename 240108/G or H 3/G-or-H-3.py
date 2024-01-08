3284565d16310f18399b3f83b51e5b2e623225b6
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-08 14:13:57 +0900

    Title: G or H 3, Time: 219ms, Memory: 31MB, Status: Passed - CodeTree

--- 240108/G or H 3/G-or-H-3.py
+++ 240108/G or H 3/G-or-H-3.py
@@ -5,6 +5,7 @@ def solution(N, K, sign_log):
     sign_log.sort(key=lambda x: int(x[0]))
     for distance, _ in sign_log:
         max_distance = max(max_distance, int(distance))
+
     pos = 1
     while pos < max_distance:
         sign = sign_log[0]
@@ -16,18 +17,21 @@ def solution(N, K, sign_log):
                 board.append(None)    
         board.append(sign[1])
         pos += diff_distance + 1
-
-    current_scope = 0
-    while current_scope < max_distance:
-        search = board[current_scope: current_scope+K+1]
-        # print(search)
+    
+    if max_distance <= K:
+        count_G = board.count("G")
+        count_H = board.count("H")
+        return count_G + count_H * 2
+        
+    for idx in range(max_distance - K):
+        search = board[idx: idx+K+1]
         count_G = search.count("G")
         count_H = search.count("H")
         temp_sum = count_G + count_H * 2
         # print(search, temp_sum)
         max_score = max(max_score, temp_sum)
-        current_scope = current_scope+K+1
     return max_score
+
 N, K = map(int, input().split())
 sign_log = [list(input().split()) for _ in range(N)]
 print(solution(N, K, sign_log))

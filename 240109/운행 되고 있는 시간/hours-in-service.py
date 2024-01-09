a17268d167b271deec78689e1ece58e9b23c9bc1
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-09 15:54:29 +0900

    Title: 운행 되고 있는 시간, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/운행 되고 있는 시간/hours-in-service.py
+++ 240109/운행 되고 있는 시간/hours-in-service.py
@@ -3,13 +3,14 @@ def solution(N, hours):
     for i in range(N):
         min_start = 1001
         max_end = 0
+        time_card = [0] * 1001
         for j in range(N):
             if i == j:
                 continue
             start, end = hours[j]
-            min_start = min(min_start, start)
-            max_end = max(max_end, end)
-        max_run_time = max(max_run_time, max_end - min_start - 1)
+            for time in range(start, end):
+                time_card[time] += 1
+        max_run_time = max(max_run_time, 1001- time_card.count(0))
     return max_run_time
 
     

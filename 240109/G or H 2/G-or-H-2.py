35dfcdf2a28912c0640d590eed6ce22618f507a8
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-09 10:13:04 +0900

    Title: G or H 2, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240109/G or H 2/G-or-H-2.py
+++ 240109/G or H 2/G-or-H-2.py
@@ -1,30 +1,21 @@
 def solution(N, sign):
     max_size = 0
-    max_socre = 0
-    for size in range(2, len(sign)):
-        for idx in range(len(sign) - size + 1):
-            search = sign[idx : idx + size]
+    for start in range(101):
+        for end in range(start+1, 101):
+            if sign[start] == 0 or sign[end] == 0:
+                continue
+
+            search = sign[start : start + end]
             G_cnt = search.count("G")
             H_cnt = search.count("H")
-            if G_cnt > 1 and H_cnt == 0 or\
-            G_cnt == 0 and H_cnt > 1 or\
-            G_cnt == H_cnt and G_cnt != 0 and H_cnt != 0 and\
-            max_socre < G_cnt + H_cnt:
-                # print(max_socre, G_cnt + H_cnt)
-                max_size = max(max_size, size-1)
-                max_socre = max(max_socre, G_cnt + H_cnt)
+            if G_cnt == 0 or H_cnt == 0 or G_cnt == H_cnt:
+                max_size = max(max_size, end - start)
+
     return max_size
 
 
 N = int(input())
-sign = [None] * 101
+sign = [0] * 101
 for idx, ch in list(input().split() for _ in range(N)):
     sign[int(idx)] = ch
-sign.reverse()
-if sign.count("H") != 0 and sign.count("G") != 0:
-    pos_H = sign.index("H")
-    pos_G = sign.index("G")
-    sign = sign[pos_G:] if pos_H > pos_G else sign[pos_H:]
-    # print(sign)
-sign.reverse()
 print(solution(N, sign))

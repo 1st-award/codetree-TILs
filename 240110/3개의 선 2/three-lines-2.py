4962bc2644f730d5829ef27119dbd66d2554a992
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-10 13:48:16 +0900

    Title: 3개의 선 2, Time: 158ms, Memory: 26MB, Status: Passed - CodeTree

--- 240110/3개의 선 2/three-lines-2.py
+++ 240110/3개의 선 2/three-lines-2.py
@@ -1,34 +1,65 @@
-# 
-def solution(N, point_list):
-    xs, xm = 0, 0
-    ys, ym = 0, 0
-    for x, y in point_list:
-        xs = min(xs, x + 1)
-        xm = max(xm, x + 1)
-        ys = min(ys, y + 1)
-        ym = max(ym, y + 1)
-    
-    max_board = max(xm, ym)
-    board = [[0] * max_board for _ in range(max_board)]
-    for x, y in point_list:
-        board[y][x] = 1
-    
-    count = 0
-    for n in range(xs, ym):
-        x_line_points = board[n]
-        y_line_points = []
-        for b in board:
-            y_line_points.append(b[n])
-        if x_line_points.count(1) > 1 or y_line_points.count(1) > 1:
-            count += 1
+MAX_X = 10
 
-    if count > 3:
-        return 0
-    else:
-        return 1
-    
-    
+# 변수 선언 및 입력
+n = int(input())
+points = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-N = int(input())
-point_list = [tuple(map(int, input().split())) for _ in range(N)]
-print(solution(N, point_list))
+ans = 0
+
+# 모든 직선에 대해 전부 시도해 봅니다.
+for i in range(MAX_X + 1):
+    for j in range(MAX_X + 1):
+        for k in range(MAX_X + 1):
+            # success : 직선 3개로 모든 점을 지나게 할 수 있으면 true
+            success = True
+            # x축에 평행한 직선 3개로
+            # 모든 점을 지나게 할 수 있는 경우
+            for x, y in points:
+                # 해당 점이 직선에 닿으면 넘어갑니다
+                if x == i or x == j or x == k:
+                    continue
+                
+                success = False
+            if success:
+                ans = 1
+
+            # x축에 평행한 직선 2개와 y축에 평행한 직선 1개로
+            # 모든 점을 지나게 할 수 있는 경우
+            success = True
+            for x, y in points:
+                # 해당 점이 직선에 닿으면 넘어갑니다
+                if x == i or x == j or y == k:
+                    continue
+                
+                success = False
+            if success:
+                ans = 1
+                
+            # x축에 평행한 직선 1개와 y축에 평행한 직선 2개로
+            # 모든 점을 지나게 할 수 있는 경우
+            success = True
+            for x, y in points:
+                # 해당 점이 직선에 닿으면 넘어갑니다
+                if x == i or y == j or y == k:
+                    continue
+                
+                success = False
+            if success:
+                ans = 1
+            
+            # y축에 평행한 직선 3개로
+            # 모든 점을 지나게 할 수 있는 경우
+            success = True
+            for x, y in points:
+                # 해당 점이 직선에 닿으면 넘어갑니다
+                if y == i or y == j or y == k:
+                    continue
+                
+                success = False
+            if success:
+                ans = 1
+
+print(ans)

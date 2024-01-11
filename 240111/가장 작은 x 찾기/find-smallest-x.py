ba0959ff778b341c699a52c67d98f69fd30ec01d
1st_award <54760813+1st-award@users.noreply.github.com>
2024-01-11 10:04:43 +0900

    Title: 가장 작은 x 찾기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240111/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,19 @@
+def solution(N, range_list):
+    for n in range(range_list[0][0], range_list[-1][1]):
+        temp = n
+        is_pass = True
+        for x, y in range_list:
+            temp *= 2
+            # print(x, y, temp)
+            if x > temp or y < temp:
+                # print("false")
+                is_pass = False
+                break
+        if is_pass:
+            return n
+    return 0
+
+N = int(input())
+range_list = [tuple(map(int, input().split())) for _ in range(N)]
+range_list.sort(key=lambda x: x[0])
+print(solution(N, range_list))

f7729a26f3b5172335816dd78933363f8b237e15
1st_award <54760813+1st-award@users.noreply.github.com>
2024-02-06 15:46:37 +0900

    Title: 초기 수열 복원하기, Time: 164ms, Memory: 11MB, Status: Passed - CodeTree

--- 240206/초기 수열 복원하기/restore-initial-sequence.java
+++ 240206/초기 수열 복원하기/restore-initial-sequence.java
@@ -20,38 +20,35 @@ public class Main {
             result[i] = Integer.parseInt(st.nextToken());
         }
 
-        String numbers = "";
-        int[] nums;
-        Set<Integer> set;
-        int start = (int) Math.pow(10, (N - 1));
-        int end = (int) Math.pow(10, N);
-        for (int i = start; i < end; ++ i) {
-            nums = str2IntArr(String.valueOf(i));
-            set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-            if (isCorrect(nums, result, N) && set.size() == N && !set.contains(0)) {
-                for (int num : nums) System.out.print(num + " ");
-                break;
+        boolean[] useNumber;
+        boolean satisfied;
+        int[] number = new int[N];
+        for (int firstN = 1; firstN <= N; ++firstN) {
+            // 추측하는 수열
+            number[0] = firstN;
+            for (int idx = 0; idx < N - 1; ++idx) {
+                number[idx + 1] = result[idx] - number[idx];
+            }
+            // 문제 조건에 맞는지 검사
+            satisfied = true;
+            useNumber = new boolean[N];
+            for (int idx = 0; idx < N; ++idx) {
+                if(number[idx] < 1 || number[idx] > N) {
+                    satisfied = false;
+                    break;
+                } else {
+                    if(useNumber[number[idx] - 1]) {
+                        satisfied = false;
+                        break;
+                    }
+                    useNumber[number[idx] - 1] = true;
+                }
+            }
+
+            if(satisfied) {
+                for (int num : number) System.out.print(num + " ");
+                System.exit(0);
             }
         }
-    }
-
-    private static int[] str2IntArr(String _str) {
-        int[] nums = new int[_str.length()];
-        char[] strs = _str.toCharArray();
-        for (int idx = 0; idx < _str.length(); ++idx) {
-            nums[idx] = strs[idx] - '0';
-        }   
-        return nums;
-    }
-
-    private static boolean isCorrect(int[] _nums, int[] _result, int N) {
-        // System.out.println(Arrays.toString(_nums));
-        // System.out.println(Arrays.toString(_result));
-        for (int idx = 0; idx < N - 1; ++ idx) {
-            if (_result[idx] != _nums[idx] + _nums[idx + 1]) {
-                return false;
-            }
-        }
-        return true;
     }
 }

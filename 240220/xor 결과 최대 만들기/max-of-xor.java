d256fb42b848ef8877b5020ad73d18a68098fe6b
1st_award <54760813+1st-award@users.noreply.github.com>
2024-02-20 11:20:34 +0900

    Title: xor 결과 최대 만들기, Time: 105ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240220/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+    private static int n, m;
+    private static int maxXOR;
+    private static ArrayList<Integer> combination = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        findCombination(1, 0);
+
+        System.out.println(maxXOR);
+    }
+
+    private static int getMaxXOR () {
+        int tempXOR = 0;
+        for (int i = 0; i < combination.size(); ++i) {
+            tempXOR ^= combination.get(i);
+        }
+        return tempXOR;
+    }
+
+    private static void findCombination(int currNum, int cnt) {
+        if (currNum == n + 1) {
+            if (cnt == m) {
+                maxXOR = Math.max(maxXOR, getMaxXOR());
+            }
+            return;
+        }
+
+        combination.add(currNum);
+        findCombination(currNum + 1, cnt + 1);
+        combination.remove(combination.size() - 1);
+
+        findCombination(currNum + 1, cnt);
+    }
+}

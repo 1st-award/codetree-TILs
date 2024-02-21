04d3e137b8b760d5dd4f9efbad22edcaa7cd9532
1st_award <54760813+1st-award@users.noreply.github.com>
2024-02-21 22:27:16 +0900

    Title: 사각형 채우기, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240221/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    private static int n;
+    private static ArrayList<Integer> memo = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        memo.add(0);
+        memo.add(1);
+        memo.add(2);
+        memo.add(3);
+        for (int i = 4; i <= n; ++i) {
+            memo.add(memo.get(i - 2) + memo.get(i - 1));
+        }
+
+        System.out.println(memo.get(n));
+    }
+}

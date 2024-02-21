6a5b17dd0ed97c6ffd50e67ad6e53ebf1e8cec2d
1st_award <54760813+1st-award@users.noreply.github.com>
2024-02-21 20:34:12 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 301ms, Memory: 53MB, Status: Passed - CodeTree

--- 240221/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 240221/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -14,11 +14,11 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         board = new int[(n * 2) + 1];
         visited = new boolean[(n * 2) + 1];
-        visited[1] = true;
-        queue.add(1);
+        visited[n] = true;
+        queue.add(n);
         BFS();
 
-        System.out.println(board[n]);
+        System.out.println(board[1]);
     }
 
     private static boolean isOutOfRange(int x) {
@@ -34,15 +34,20 @@ public class Main {
     private static void BFS() {
         while (queue.isEmpty() == false) {
             int originN = queue.poll();
-            int[] dn = new int[]{-1, 1, originN, originN * 2};
 
             for (int i = 0; i < 4; ++i) {
-                int newN = originN + dn[i];
+                int newN = 0;
+
+                if (i == 0) newN = originN - 1;
+                else if (i == 1) newN = originN + 1;
+                else if (i == 2 && originN % 2 == 0) newN = originN / 2;
+                else if (i == 3 && originN % 3 == 0) newN = originN / 3;
+                else continue;
+
 
                 if (canGo(newN)) {
                     visited[newN] = true;
                     board[newN] = board[originN] + 1;
-                    if (newN == n)  return;
                     queue.add(newN);
                 }
             }

aaa3a3d7c8edb8f682c3f957ea910a11f6c9205d
1st_award <54760813+1st-award@users.noreply.github.com>
2024-05-01 22:07:55 +0900

    Title: 장학금, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240501/장학금/scholarship.java
+++ 240501/장학금/scholarship.java
@@ -9,9 +9,9 @@ public class Main {
 
         if (middleScore >= 90) {
             if (finalScore >= 95) {
-                System.out.println(10);
+                System.out.println(100000);
             } else if (finalScore >= 90) {
-                System.out.println(5);
+                System.out.println(50000);
             } else {
                 System.out.println(0);
             }

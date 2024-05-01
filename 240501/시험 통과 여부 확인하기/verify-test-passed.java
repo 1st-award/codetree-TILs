bf3d76fa52aa52e2d1009352b5cb232563747bbe
1st_award <54760813+1st-award@users.noreply.github.com>
2024-05-01 17:16:35 +0900

    Title: 시험 통과 여부 확인하기, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240501/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 79) {
+            System.out.println("pass");
+        } else {
+            int score = 80 - n;
+            System.out.printf("%d more score\n", score);
+        }
+    }
+}

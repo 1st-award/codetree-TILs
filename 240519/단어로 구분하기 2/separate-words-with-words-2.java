ad35e20be743073006bc924941d5e73919c99bb4
1st_award <54760813+1st-award@users.noreply.github.com>
2024-05-20 00:00:06 +0900

    Title: 단어로 구분하기 2, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240519/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    private final static int SIZE = 10;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[SIZE];
+
+        for (int i = 0; i < SIZE; ++i) {
+            arr[i] = sc.next();
+        }
+
+        for (int i = 0; i < SIZE; i += 2) {
+            System.out.println(arr[i]);
+        }
+    }
+}

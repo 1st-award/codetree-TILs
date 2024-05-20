4e34a493c3620297a282305c99eeaf854990f110
1st_award <54760813+1st-award@users.noreply.github.com>
2024-05-20 11:32:45 +0900

    Title: 그 다음 알파벳, Time: 187ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char alpha = sc.next().charAt(0);
+
+        char nextAlpha = (char)((int)((alpha - 'a' + 1) % ('z' - 'a' + 1) + 'a'));
+        System.out.println(nextAlpha);
+    }
+}

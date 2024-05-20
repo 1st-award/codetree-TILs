90bf2f4d821db235e475ba6ba42314aa2d83504d
1st_award <54760813+1st-award@users.noreply.github.com>
2024-05-20 10:29:02 +0900

    Title: 두 번째를 첫 번째로, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] strArr = str.toCharArray();
+        char firstChar = strArr[0];
+        char secondChar = strArr[1];
+
+        for (int i = 0; i < strArr.length; ++i) {
+            if (strArr[i] == secondChar) {
+                strArr[i] = firstChar;
+            }
+        }
+        String editStr = String.valueOf(strArr);
+
+        System.out.println(editStr);
+    }
+}

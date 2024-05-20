dbf3c48ae6c819ebb814150ec534b2a73186748e
1st_award <54760813+1st-award@users.noreply.github.com>
2024-05-20 19:35:15 +0900

    Title: 대문자 소문자 바꾸기, Time: 203ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); ++i) {
+
+            char chr = str.charAt(i);
+            if (chr >= 'a' && chr <= 'z') {
+                System.out.print((char) (chr - 'a' + 'A'));
+            }
+            else if (chr >= 'A' && chr <= 'Z') {
+                System.out.print((char) (chr - 'A' + 'a'));
+            }
+        }
+    }
+}
